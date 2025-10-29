824f2038934d792851ebafab4570b7c2266365f0
fadga-odoo <fadga@odoo.com>
2025-10-29 10:32:03 +0700

    add menu view for estate property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,13 +8,26 @@ class EstateProperty(models.Model):
     name = fields.Char("Title", required=True)
     description = fields.Text()
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Available From")
+    
+    next_three_months = fields.Date.today() + relativedelta(months=3)
+    date_availability = fields.Date("Available From", copy=False, default=next_three_months)
+    
     expected_price = fields.Float("Expected Price", required=True)
-    selling_price = fields.Float("Selling Price")
-    bedrooms = fields.Integer("Bedrooms")
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Bedrooms", default=2)
     living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
     garden = fields.Boolean("Garden")
     garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(selection=[("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")], string="Garden Orientation")
+
+    # Reserved
+    active = fields.Boolean(default=True)
+    state = fields.Selection(selection=[
+        ("new", "New"),
+        ("offer_received", "Offer Received"),
+        ("offer_accepted", "Offer Accepted"),
+        ("sold", "Sold"),
+        ("cancelled", "Cancelled")],
+        required=True, copy=False, default="new")

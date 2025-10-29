0e45c258958bf56bd8b85e2d15d97dee2069c30b
fadga-odoo <fadga@odoo.com>
2025-10-29 09:22:43 +0700

    add menu for estate property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,16 +4,16 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char("Property Name", required=True)
+    name = fields.Char("Title", required=True)
     description = fields.Text()
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Date Availability")
+    date_availability = fields.Date("Available From")
     expected_price = fields.Float("Expected Price", required=True)
     selling_price = fields.Float("Selling Price")
     bedrooms = fields.Integer("Bedrooms")
-    living_area = fields.Integer("Living Area")
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
     garden = fields.Boolean("Garden")
-    garden_area = fields.Integer("Garden Area")
+    garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(selection=[("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")], string="Garden Orientation")

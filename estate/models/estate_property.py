75faade278a3800cc648c6404f9181209e0d097f
fadga-odoo <fadga@odoo.com>
2025-10-29 16:31:41 +0700

    [CHA08] add computed field total_area

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
@@ -8,10 +8,8 @@ class EstateProperty(models.Model):
     name = fields.Char("Title", required=True)
     description = fields.Text()
     postcode = fields.Char("Postcode")
-    
     next_three_months = fields.Date.today() + relativedelta(months=3)
     date_availability = fields.Date("Available From", copy=False, default=next_three_months)
-    
     expected_price = fields.Float("Expected Price", required=True)
     selling_price = fields.Float("Selling Price", readonly=True, copy=False)
     bedrooms = fields.Integer("Bedrooms", default=2)
@@ -22,14 +20,20 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(selection=[("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")], string="Garden Orientation")
     
+    # ---------
     # Reference
+    # ---------
+    
     type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer = fields.Many2one("res.partner", string="Buyer", copy="False")
     salesperson = fields.Many2one("res.partner", string="Salesperson", default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag", string="Property Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
 
+    # --------
     # Reserved
+    # --------
+    
     active = fields.Boolean(default=True)
     state = fields.Selection(selection=[
         ("new", "New"),
@@ -38,3 +42,14 @@ class EstateProperty(models.Model):
         ("sold", "Sold"),
         ("cancelled", "Cancelled")],
         required=True, copy=False, default="new")
+
+    # --------    
+    # Computed
+    # --------
+
+    total_area = fields.Integer(compute="_compute_total_area")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area

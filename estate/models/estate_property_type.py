404430faa0900fe765d9dcf152c2a54b3e0e0a67
fadga-odoo <fadga@odoo.com>
2025-10-30 11:50:43 +0700

    11 add manual order to property types in list view

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -10,4 +10,5 @@ class EstatePropertyType(models.Model):
     _order = "name asc"
 
     name = fields.Char("Name", required=True)
+    sequence = fields.Char("Sequence", default=1, help="Used to order stages, lower is better.")
     property_ids = fields.One2many("estate.property", "type_id", string="Properties")

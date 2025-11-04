36a4b3f6fb0b404fa3358a059e98c058ad4e7f44
fadga-odoo <fadga@odoo.com>
2025-11-04 15:51:39 +0700

    finish chapter build a PDF

---
+++ custom_contact/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Custom Contact",
+    "description": "Custom contact-nya kakak?",
+    "summary": "Custom contact-nya kakak?",
+    "depends": [
+        "base",
+        "web",
+    ],
+    "data": [
+        "report/contact_reports.xml",
+        "report/contact_templates.xml"
+    ],
+    "installable": True,
+    "application": True,
+    "author": "Dariel"
+}

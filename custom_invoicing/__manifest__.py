4dba3afadd9d5ebaac5ff5ea88794ad5bf7f736a
fadga-odoo <fadga@odoo.com>
2025-11-04 10:42:35 +0700

    case study: modify invoice report

---
+++ custom_invoicing/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Custom Invoicing",
+    "description": "Custom invoice-nya kakak?",
+    "summary": "Custom invoice-nya kakak?",
+    "depends": [
+        "base",
+        "account",
+    ],
+    "data": [
+        "report/invoice_report.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "author": "Dariel"
+}

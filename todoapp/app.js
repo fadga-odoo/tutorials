4178a8a2bf17d6bba84a3061a32ed6ae8b78e725
fadga-odoo <fadga@odoo.com>
2025-11-06 09:56:47 +0700

    understanding how props and state within scope works

--- todoapp/app.js
+++ todoapp/app.js
@@ -1,21 +1,28 @@
-const { Component, mount, xml } = owl;
+const { Component, mount, xml, useState } = owl;
+
+class Task extends Component {
+    static template = xml`
+    <div class="task-container" t-att-class="props.tasky.isCompleted ? 'done' : ''">
+        <input type="checkbox" t-att-checked="props.tasky.isCompleted"/>
+        <span><t t-esc="props.tasky.text"/></span>
+    </div>`;
+    static props = ["tasky"]; // Used inside the tag: <Task taskItem="value">, meaning that we assign "value" to taskItem variable within the scope.
+}
 
 class Root extends Component {
-/**
- * t-foreach is used for looping: https://github.com/odoo/owl/blob/master/doc/reference/templates.md#loops
- * t-att is used for dynamic attribute: https://github.com/odoo/owl/blob/master/doc/reference/templates.md#dynamic-attributes
- * More about: https://github.com/odoo/owl/blob/master/doc/reference/templates.md#directives
- */
+  /**
+   * t-foreach is used for looping: https://github.com/odoo/owl/blob/master/doc/reference/templates.md#loops* t-att is used for dynamic attribute: https://github.com/odoo/owl/blob/master/doc/reference/templates.md#dynamic-attributes
+   * t-att is used for dynamic attribute: https://github.com/odoo/owl/blob/master/doc/reference/templates.md#dynamic-attributes
+   * More about: https://github.com/odoo/owl/blob/master/doc/reference/templates.md#directives
+   */
   static template = xml`
     <div class="task-list">
-        <t t-foreach="tasks" t-as="task" t-key="task.id">
-            <div class="task" t-att-class="task.isCompleted ? 'done' : ''">
-                <input type="checkbox" t-att-checked="task.isCompleted"/>
-                <span><t t-esc="task.text"/></span>
-            </div>
+        <t t-foreach="tasks" t-as="taskItem" t-key="taskItem.id">
+            <Task tasky="taskItem"/>
         </t>
     </div>`;
-
+  static components = { Task };
+  
   tasks = [
     {
       id: 1,
@@ -26,7 +33,7 @@ class Root extends Component {
       id: 2,
       text: "clean house",
       isCompleted: false,
-    },
+    }
   ];
 }
 

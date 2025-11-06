012c3bc964a46d5dd9881b8838a27749979065e8
fadga-odoo <fadga@odoo.com>
2025-11-06 11:43:15 +0700

    fetch user input and use it in a function

--- todoapp/app.js
+++ todoapp/app.js
@@ -1,4 +1,4 @@
-const { Component, mount, xml, useState } = owl;
+const { Component, mount, xml, useRef, onMounted } = owl;
 
 class Task extends Component {
     static template = xml`
@@ -6,7 +6,7 @@ class Task extends Component {
         <input type="checkbox" t-att-checked="props.task.isCompleted"/>
         <span><t t-esc="props.task.text"/></span>
     </div>`;
-    static props = ["task"]; // Used inside the tag: <Task taskItem="value">, meaning that we assign "value" to taskItem variable within the scope (accessed by props.taskItem).
+    static props = ["task"]; // Used inside the tag: <Task taskItem="value">, meaning that we assign "value" to taskItem variable (property) within the scope (accessed by props.taskItem).
 }
 
 class Root extends Component {
@@ -16,12 +16,20 @@ class Root extends Component {
    * More about: https://github.com/odoo/owl/blob/master/doc/reference/templates.md#directives
    */
   static template = xml`
-    <div class="task-list">
-        <t t-foreach="tasks" t-as="task" t-key="task.id">
-            <Task task="task"/>
-        </t>
+    <div class="todo-app">
+      <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
+      <div class="task-list">
+          <t t-foreach="tasks" t-as="task" t-key="task.id">
+              <Task task="task"/>
+          </t>
+      </div>
     </div>`;
   static components = { Task };
+
+  setup() {
+    const inputRef = useRef("add-input");
+    onMounted(() => inputRef.el.focus());
+  }
   
   tasks = [
     {
@@ -35,6 +43,18 @@ class Root extends Component {
       isCompleted: false,
     }
   ];
+
+  addTask(event) {
+    if (event.keyCode === 13) {
+      /**
+       * This is how we fetch user input.
+       * KeyboardEvent (event) will be sent to the browser and user input is stored in event.target.value.
+       */
+      const text = event.target.value.trim();
+      event.target.value = "";
+      console.log('adding task', text);
+    }
+  }
 }
 
 mount(Root, document.body);
